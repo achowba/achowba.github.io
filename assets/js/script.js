d8bf5554fee2cd717c6b28767ff9f4f28b5a4a01
Achowba <princeatauba@gmail.com>
2019-04-12 15:04:21 +0400

    added night mode

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+const toggleSwitch = document.querySelector('#toggleDarkMode');
+const currentTheme = localStorage.getItem('theme');
+// const logoImage = document.querySelector("#logo"); // logoImage.src = "../assets/img/logo_dark.png";
+
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+    }
+}
+
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
+    } else {
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
+    }
+}
+
+toggleSwitch.addEventListener('change', switchTheme, false);
