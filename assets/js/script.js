52d373bfd361a2a98672aae98385bf5181735b25
achowba ­­­­ ­­ ­­ <achowba>
2020-06-07 18:51:48 +0400

    remove resume and projects

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 const toggleSwitch = document.querySelector('#toggleDarkMode');
+const toggleButtonWrapper = document.querySelector('#toggleButton');
 const currentTheme = localStorage.getItem('theme');
 
 if (currentTheme) {
@@ -6,16 +7,29 @@ if (currentTheme) {
 
     if (currentTheme === 'dark') {
         toggleSwitch.checked = true;
+        changeTitle('dark');
+    } else {
+        changeTitle('light');
     }
 }
 
 function switchTheme(e) {
     if (e.target.checked) {
         document.documentElement.setAttribute('data-theme', 'dark');
+        changeTitle('dark');
         localStorage.setItem('theme', 'dark');
     } else {
         document.documentElement.setAttribute('data-theme', 'light');
+        changeTitle('light');
         localStorage.setItem('theme', 'light');
+    }
+}
+
+function changeTitle (theme) {
+    if (theme === 'dark') {
+        document.documentElement.setAttribute('title', 'Bravo Six, Going Bright');
+    } else if (theme === 'light') {
+        document.documentElement.setAttribute('title', 'Bravo Six, Going Dark');
     }
 }
 
