73a105c650954f38ead82f7c8d08a4ce41a7baa7
Prince <justprince9@gmail.com>
2022-07-29 00:15:10 +0400

    Updated icons
    1. Updated icons
    2. Updated link styles

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
+const currentTheme = localStorage.getItem('theme');
+const logoDarkImg = document.querySelector('#logo-dark');
+const logoLightImg = document.querySelector('#logo-light');
 const toggleSwitch = document.querySelector('#toggleDarkMode');
 const toggleButtonWrapper = document.querySelector('#toggleButton');
-const currentTheme = localStorage.getItem('theme');
 
 if (currentTheme) {
     document.documentElement.setAttribute('data-theme', currentTheme);
@@ -17,19 +19,31 @@ function switchTheme(e) {
     if (e.target.checked) {
         document.documentElement.setAttribute('data-theme', 'dark');
         changeTitle('dark');
+        changeImg('dark');
         localStorage.setItem('theme', 'dark');
     } else {
         document.documentElement.setAttribute('data-theme', 'light');
         changeTitle('light');
+        changeImg('light');
         localStorage.setItem('theme', 'light');
     }
 }
 
-function changeTitle (theme) {
+function changeTitle(theme) {
     if (theme === 'dark') {
         toggleButtonWrapper.setAttribute('title', 'Bravo Six, Going Bright');
     } else if (theme === 'light') {
         toggleButtonWrapper.setAttribute('title', 'Bravo Six, Going Dark');
+    }
+}
+
+function changeImg(theme) {
+    if (theme === 'dark') {
+        logoDarkImg.style.display = 'unset';
+        logoLightImg.style.display = 'none';
+    } else if (theme === 'light') {
+        logoDarkImg.style.display = 'none';
+        logoLightImg.style.display = 'unset';
     }
 }
 
