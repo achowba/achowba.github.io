7d14187b5f2cc92eea79b58e1b7aff90d70fe993
Prince <justprince9@gmail.com>
2024-07-08 08:16:39 +0100

    feat (logo): Added crown to logo

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,50 +1,75 @@
-const currentTheme = localStorage.getItem('theme');
+const currentTheme = getTheme();
 const logoDarkImg = document.querySelector('#logo-dark');
 const logoLightImg = document.querySelector('#logo-light');
 const toggleSwitch = document.querySelector('#toggleDarkMode');
 const toggleButtonWrapper = document.querySelector('#toggleButton');
 
 if (currentTheme) {
-    document.documentElement.setAttribute('data-theme', currentTheme);
-
-    if (currentTheme === 'dark') {
-        toggleSwitch.checked = true;
-        changeTitle('dark');
-    } else {
-        changeTitle('light');
-    }
+    setTheme(currentTheme);
+    toggleSwitch.checked = currentTheme === 'dark';
 }
 
-function switchTheme(e) {
+function toggleTheme(e) {
     if (e.target.checked) {
-        document.documentElement.setAttribute('data-theme', 'dark');
-        changeTitle('dark');
-        changeImg('dark');
-        localStorage.setItem('theme', 'dark');
-    } else {
-        document.documentElement.setAttribute('data-theme', 'light');
-        changeTitle('light');
-        changeImg('light');
-        localStorage.setItem('theme', 'light');
+        setTheme('dark');
+        return;
     }
+
+    setTheme('light');
 }
 
 function changeTitle(theme) {
     if (theme === 'dark') {
         toggleButtonWrapper.setAttribute('title', 'Bravo Six, Going Bright');
-    } else if (theme === 'light') {
-        toggleButtonWrapper.setAttribute('title', 'Bravo Six, Going Dark');
+        return;
     }
+
+    toggleButtonWrapper.setAttribute('title', 'Bravo Six, Going Dark');
 }
 
 function changeImg(theme) {
     if (theme === 'dark') {
         logoDarkImg.style.display = 'unset';
         logoLightImg.style.display = 'none';
-    } else if (theme === 'light') {
+    }
+
+    if (theme === 'light') {
         logoDarkImg.style.display = 'none';
         logoLightImg.style.display = 'unset';
     }
 }
 
-toggleSwitch.addEventListener('change', switchTheme, false);
+function getTheme() {
+    const currentTheme = localStorage.getItem('theme');
+
+    if (currentTheme) {
+        return currentTheme;
+    }
+
+    if (window.matchMedia) {
+        if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+            return 'dark';
+        } else {
+            return 'light';
+        }
+    }
+
+    return 'light';
+}
+
+function setTheme(theme) {
+    changeImg(theme);
+    changeTitle(theme);
+
+    localStorage.setItem('theme', theme);
+    document.documentElement.setAttribute('data-theme', theme);
+}
+
+// event listeners
+document.addEventListener('DOMContentLoaded', () => {
+    const currentTheme = getTheme();
+
+    setTheme(currentTheme);
+    changeImg(currentTheme);
+});
+toggleSwitch.addEventListener('change', toggleTheme, false);
